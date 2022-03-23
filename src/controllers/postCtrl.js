a1aaf14a146f448ccbd29b2d2fd325b8299eb91f
snguyen70 <sangngoc.nguyen@dxc.com>
2022-03-23 16:41:07 +0700

    convert tags to keyword

--- src/controllers/postCtrl.js
+++ src/controllers/postCtrl.js
@@ -1,273 +1,257 @@
+import Comment from '../models/Comment.js';
 import Post from '../models/Post.js';
 import User from '../models/User.js';
-import Comment from '../models/Comment.js';
 import { getPostResponse } from '../utils/mongoose.js';
 
 async function getAll(req, res) {
-	try {
-		const params = req.query;
+  try {
+    const params = req.query;
 
-		const getFilter = ({ tag, username }) => {
-			if (tag && username)
-				return { 'tags.value': tag, 'author.username': username };
-			if (tag) return { 'tags.value': tag };
-			if (username) return { 'author.username': username };
+    const getFilter = ({ keyword, username }) => {
+      if (keyword && username) return { 'keyword.value': keyword, 'author.username': username };
+      if (keyword) return { 'keyword.value': keyword };
+      if (username) return { 'author.username': username };
 
-			return {};
-		};
+      return {};
+    };
 
-		const filter = getFilter(params);
-		const postResponse = await getPostResponse(filter, params);
+    const filter = getFilter(params);
+    const postResponse = await getPostResponse(filter, params);
 
-		res.send(postResponse);
-	} catch (error) {
-		res.status(500).send(error);
-	}
+    res.send(postResponse);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 async function getBySlug(req, res) {
-	try {
-		const { postSlug } = req.params;
+  try {
+    const { postSlug } = req.params;
 
-		const post = await Post.findOne({ slug: postSlug }).lean();
-		if (!post)
-			return res.status(404).send({ message: 'Bài viết không tồn tại' });
+    const post = await Post.findOne({ slug: postSlug }).lean();
+    if (!post) return res.status(404).send({ message: 'Bài viết không tồn tại' });
 
-		const commentCount = await Comment.countDocuments({ postId: post._id });
+    const commentCount = await Comment.countDocuments({ postId: post._id });
 
-		res.send({ ...post, commentCount });
-	} catch (error) {
-		res.status(500).send(error);
-	}
+    res.send({ ...post, commentCount });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 async function getForEdit(req, res) {
-	try {
-		const { postId } = req.params;
-		const user = req.user;
+  try {
+    const { postId } = req.params;
+    const user = req.user;
 
-		const post = await Post.findById(postId).lean();
-		if (!post)
-			return res.status(404).send({ message: 'bài viết không tồn tại' });
+    const post = await Post.findById(postId).lean();
+    if (!post) return res.status(404).send({ message: 'bài viết không tồn tại' });
 
-		if (user.role !== 'admin' && !post.authorId.equals(user._id))
-			return res
-				.status(403)
-				.send({ message: 'Bạn không có quyền chỉnh sửa bài viết này' });
+    if (user.role !== 'admin' && !post.authorId.equals(user._id))
+      return res.status(403).send({ message: 'Bạn không có quyền chỉnh sửa bài viết này' });
 
-		res.send(post);
-	} catch (error) {
-		res.status(500).send(error);
-	}
+    res.send(post);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 async function getMyPostList(req, res) {
-	try {
-		const params = req.query;
-		const user = req.user;
+  try {
+    const params = req.query;
+    const user = req.user;
 
-		const filter = { authorId: user._id };
-		const postResponse = await getPostResponse(filter, params);
+    const filter = { authorId: user._id };
+    const postResponse = await getPostResponse(filter, params);
 
-		res.send(postResponse);
-	} catch (error) {
-		res.status(500).send(error);
-	}
+    res.send(postResponse);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 async function getSavedList(req, res) {
-	try {
-		const params = req.query;
-		const { saved } = req.user;
+  try {
+    const params = req.query;
+    const { saved } = req.user;
 
-		const filter = { _id: { $in: saved } };
-		const postResponse = await getPostResponse(filter, params);
+    const filter = { _id: { $in: saved } };
+    const postResponse = await getPostResponse(filter, params);
 
-		res.send(postResponse);
-	} catch (error) {
-		res.status(500).send(error);
-	}
+    res.send(postResponse);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 async function create(req, res) {
-	try {
-		const formData = req.body;
-		const { _id, name, avatar } = req.user;
+  try {
+    const formData = req.body;
+    const { _id, name, avatar } = req.user;
 
-		const newPost = new Post({
-			...formData,
-			author: {
-				_id,
-				name,
-				avatar,
-			},
-		});
+    const newPost = new Post({
+      ...formData,
+      author: {
+        _id,
+        name,
+        avatar,
+      },
+    });
 
-		const savedPost = await newPost.save();
+    const savedPost = await newPost.save();
 
-		res.send(savedPost?._doc);
-	} catch (error) {
-		console.log('~ error', error);
-		res.status(500).send(error);
-	}
+    res.send(savedPost?._doc);
+  } catch (error) {
+    console.log('~ error', error);
+    res.status(500).send(error);
+  }
 }
 
 async function update(req, res) {
-	try {
-		const { postId } = req.params;
-		const formData = req.body;
-		const user = req.user;
+  try {
+    const { postId } = req.params;
+    const formData = req.body;
+    const user = req.user;
 
-		const post = await Post.findById(postId).lean();
-		if (!post)
-			return res.status(404).send({ message: 'Bài viết không tồn tại' });
+    const post = await Post.findById(postId).lean();
+    if (!post) return res.status(404).send({ message: 'Bài viết không tồn tại' });
 
-		if (user.role !== 'admin' && !post.authorId.equals(user._id))
-			return res.status(403).send('Bạn không có quyền chỉnh sửa bài viết này');
+    if (user.role !== 'admin' && !post.authorId.equals(user._id))
+      return res.status(403).send('Bạn không có quyền chỉnh sửa bài viết này');
 
-		const updatedPost = await Post.findByIdAndUpdate(
-			postId,
-			{ $set: formData },
-			{ new: true }
-		).lean();
+    const updatedPost = await Post.findByIdAndUpdate(
+      postId,
+      { $set: formData },
+      { new: true }
+    ).lean();
 
-		res.send(updatedPost);
-	} catch (error) {
-		res.status(500).send(error);
-	}
+    res.send(updatedPost);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 async function remove(req, res) {
-	try {
-		const { postId } = req.params;
-		const user = req.user;
+  try {
+    const { postId } = req.params;
+    const user = req.user;
 
-		const post = await Post.findById(postId).lean();
-		if (!post)
-			return res.status(404).send({ message: 'Bài viết không tồn tại' });
+    const post = await Post.findById(postId).lean();
+    if (!post) return res.status(404).send({ message: 'Bài viết không tồn tại' });
 
-		if (user.role !== 'admin' && !post.authorId.equals(user._id))
-			return res.status(403).send('Bạn không có quyền xóa bài viết này');
+    if (user.role !== 'admin' && !post.authorId.equals(user._id))
+      return res.status(403).send('Bạn không có quyền xóa bài viết này');
 
-		await Post.deleteOne({ _id: postId });
-		await Comment.deleteMany({ postId });
-		await User.updateMany(
-			{ saved: { $in: [postId] } },
-			{ $pull: { saved: postId } }
-		);
+    await Post.deleteOne({ _id: postId });
+    await Comment.deleteMany({ postId });
+    await User.updateMany({ saved: { $in: [postId] } }, { $pull: { saved: postId } });
 
-		res.sendStatus(200);
-	} catch (error) {
-		res.status(500).send(error);
-	}
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 async function like(req, res) {
-	try {
-		const { postId } = req.params;
-		const { _id: userId } = req.user;
+  try {
+    const { postId } = req.params;
+    const { _id: userId } = req.user;
 
-		const post = await Post.findById(postId);
-		if (!post)
-			return res.status(404).send({ message: 'Bài viết không tồn tại' });
+    const post = await Post.findById(postId);
+    if (!post) return res.status(404).send({ message: 'Bài viết không tồn tại' });
 
-		const isLiked = post.likes.some((id) => id.equals(userId));
+    const isLiked = post.likes.some((id) => id.equals(userId));
 
-		const update = isLiked
-			? { $pull: { likes: userId } }
-			: { $push: { likes: userId } };
+    const update = isLiked ? { $pull: { likes: userId } } : { $push: { likes: userId } };
 
-		const updatedPost = await Post.findByIdAndUpdate(postId, update, {
-			new: true,
-		}).lean();
+    const updatedPost = await Post.findByIdAndUpdate(postId, update, {
+      new: true,
+    }).lean();
 
-		res.send(updatedPost);
-	} catch (error) {
-		res.status(500).send(error);
-	}
+    res.send(updatedPost);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 async function save(req, res) {
-	try {
-		const { postId } = req.params;
-		const user = req.user;
+  try {
+    const { postId } = req.params;
+    const user = req.user;
 
-		const post = await Post.findById(postId).lean();
-		if (!post)
-			return res.status(404).send({ message: 'Bài viết không tồn tại' });
+    const post = await Post.findById(postId).lean();
+    if (!post) return res.status(404).send({ message: 'Bài viết không tồn tại' });
 
-		if (user.saved.includes(postId)) {
-			return res.status(400).send({ message: 'Bài viết đã được lưu' });
-		}
+    if (user.saved.includes(postId)) {
+      return res.status(400).send({ message: 'Bài viết đã được lưu' });
+    }
 
-		await User.updateOne({ _id: user._id }, { $push: { saved: postId } });
+    await User.updateOne({ _id: user._id }, { $push: { saved: postId } });
 
-		res.sendStatus(200);
-	} catch (error) {
-		res.status(500).send(error);
-	}
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 async function unSave(req, res) {
-	try {
-		const { postId } = req.params;
-		const user = req.user;
+  try {
+    const { postId } = req.params;
+    const user = req.user;
 
-		const post = await Post.findById(postId).lean();
-		if (!post)
-			return res.status(404).send({ message: 'Bài viết không tồn tại' });
+    const post = await Post.findById(postId).lean();
+    if (!post) return res.status(404).send({ message: 'Bài viết không tồn tại' });
 
-		const indexOfPostId = user.saved.findIndex((id) => id === postId);
-		if (indexOfPostId < 0)
-			return res.status(400).send({ message: 'Bài viết chưa được lưu' });
+    const indexOfPostId = user.saved.findIndex((id) => id === postId);
+    if (indexOfPostId < 0) return res.status(400).send({ message: 'Bài viết chưa được lưu' });
 
-		const savedPosts = user?.saved;
-		savedPosts.splice(indexOfPostId, 1);
+    const savedPosts = user?.saved;
+    savedPosts.splice(indexOfPostId, 1);
 
-		await User.updateOne(
-			{ _id: user._id },
-			{
-				$set: { saved: savedPosts },
-			},
-			{ new: true }
-		);
+    await User.updateOne(
+      { _id: user._id },
+      {
+        $set: { saved: savedPosts },
+      },
+      { new: true }
+    );
 
-		res.sendStatus(200);
-	} catch (error) {
-		res.status(500).send(error);
-	}
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 async function search(req, res) {
-	try {
-		const { q: searchTerm } = req.query;
+  try {
+    const { q: searchTerm } = req.query;
 
-		const postList = await Post.find({
-			slug: {
-				$regex: new RegExp(searchTerm),
-				$options: 'i',
-			},
-		}).sort({ createdAt: -1 });
+    const postList = await Post.find({
+      slug: {
+        $regex: new RegExp(searchTerm),
+        $options: 'i',
+      },
+    }).sort({ createdAt: -1 });
 
-		return res.send(postList);
-	} catch (error) {
-		res.status(500).send(error);
-	}
+    return res.send(postList);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 const postCtrl = {
-	getAll,
-	getBySlug,
-	getForEdit,
-	getMyPostList,
-	getSavedList,
-	create,
-	update,
-	remove,
-	like,
-	save,
-	unSave,
-	search,
+  getAll,
+  getBySlug,
+  getForEdit,
+  getMyPostList,
+  getSavedList,
+  create,
+  update,
+  remove,
+  like,
+  save,
+  unSave,
+  search,
 };
 
 export default postCtrl;
--- src/models/Post.js
+++ src/models/Post.js
@@ -4,54 +4,54 @@ import slug from 'mongoose-slug-updater';
 mongoose.plugin(slug);
 
 const postSchema = new mongoose.Schema(
-	{
-		title: {
-			type: String,
-			required: true,
-		},
-		content: {
-			type: String,
-			required: true,
-		},
-		description: {
-			type: String,
-			default: '',
-		},
-		thumbnail: {
-			type: String,
-			default: '',
-		},
-		authorId: {
-			type: mongoose.Schema.Types.ObjectId,
-			required: true,
-		},
-		author: {
-			_id: mongoose.Schema.Types.ObjectId,
-			name: String,
-			avatar: String,
-			username: String,
-		},
-		tags: {
-			type: [{ name: String, value: String }],
-			default: [],
-		},
-		likes: {
-			type: [mongoose.Schema.Types.ObjectId],
-			default: [],
-		},
-		commentCount: {
-			type: Number,
-			default: 0,
-		},
-		slug: {
-			type: String,
-			slug: 'title',
-			unique: true,
-		},
-	},
-	{
-		timestamps: true,
-	}
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+    thumbnail: {
+      type: String,
+      default: '',
+    },
+    authorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    author: {
+      _id: mongoose.Schema.Types.ObjectId,
+      name: String,
+      avatar: String,
+      username: String,
+    },
+    keywords: {
+      type: [{ name: String, value: String }],
+      default: [],
+    },
+    likes: {
+      type: [mongoose.Schema.Types.ObjectId],
+      default: [],
+    },
+    commentCount: {
+      type: Number,
+      default: 0,
+    },
+    slug: {
+      type: String,
+      slug: 'title',
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 export default mongoose.model('Post', postSchema);
