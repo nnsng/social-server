add8eedecbe6a519ea4b9e18d22891195589e2d7
Sangg <ngocsang1201@gmail.com>
2022-03-31 11:39:10 +0700

    revert

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose';
 
-export async function connectToDatabase(uri) {
-  try {
-    await mongoose.connect(uri, {
+export function connectToDatabase(uri) {
+  mongoose.connect(
+    uri,
+    {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    });
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    console.log('Failed to connect to MongoDB');
-    throw error;
-  }
+    },
+    (error) => {
+      if (error) throw error;
+      console.log('Connected to MongoDB');
+    }
+  );
 }
